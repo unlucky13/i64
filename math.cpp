8280a9e146ac29c90c46796228bb5c50d09882b0
unlucky13 <mazharcse@yahoo.com>
2013-08-05 09:57:19 +0600

    Update math.cpp

--- math.cpp
+++ math.cpp
@@ -1,6 +1,6 @@
 /=======================================The extended Euclidean algorithm from http://e-maxx.ru/algo/extended_euclid_algorithm====/
 
-int exgcd (int a, int b, int & x, int & y) {
+int exgcd (int a, int b, int & x, int & y) { //assuming a<b 
   if (a == 0) {
 		x = 0; y = 1;
 		return b;
